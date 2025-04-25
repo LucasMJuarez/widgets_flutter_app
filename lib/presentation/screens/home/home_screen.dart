af31b6a2ae519bf7542672d7de66dd1332f01cbb
lucas juarez <lujuarez@emeal.nttdata.com>
2025-04-25 14:31:38 -0300

    on tap with context push in navigation

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
@@ -49,20 +50,7 @@ class _CustomListTitle extends StatelessWidget {
       subtitle: Text(menuItem.subTitle),
       trailing: Icon(Icons.chevron_right_outlined, color: colors.primary),
       onTap: () {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) {
-              switch (menuItem.link) {
-                case '/buttons':
-                  return const ButtonsScreen();
-                case '/card':
-                  return const CardsScreen();
-                default:
-                  return const Placeholder(); // Fallback screen
-              }
-            },
-          ),
-        );
+        context.push(menuItem.link);
       },
     );
   }
