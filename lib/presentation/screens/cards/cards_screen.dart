b4b4ed14cc0bee326259fdccea832423b1552184
lucas juarez <lujuarez@emeal.nttdata.com>
2025-04-25 17:50:24 -0300

    add map of cards props

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+const cards = <Map<String, dynamic>>[
+  {'elevation': 0.0, 'label': 'Elevation 0.0'},
+  {'elevation': 1.0, 'label': 'Elevation 1.0'},
+  {'elevation': 2.0, 'label': 'Elevation 2.0'},
+  {'elevation': 3.0, 'label': 'Elevation 3.0'},
+  {'elevation': 4.0, 'label': 'Elevation 4.0'},
+  {'elevation': 5.0, 'label': 'Elevation 5.0'},
+];
+
 class CardsScreen extends StatelessWidget {
   static const String name = 'cards_screen';
   const CardsScreen({super.key});
@@ -14,7 +23,7 @@ class CardsScreen extends StatelessWidget {
 }
 
 class _CardsView extends StatelessWidget {
-  const _CardsView({super.key});
+  const _CardsView();
 
   @override
   Widget build(BuildContext context) {
