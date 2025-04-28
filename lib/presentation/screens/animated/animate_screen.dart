0b3e26ecddde9e4686d24686e798a46aaa30b80d
lucas juarez <lujuarez@emeal.nttdata.com>
2025-04-28 13:24:19 -0300

    add more screens for the next seccion

---
+++ lib/presentation/screens/animated/animate_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AnimateScreen extends StatelessWidget {
+  static const String name = 'animate_screen';
+
+  const AnimateScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
