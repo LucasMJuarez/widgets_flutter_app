5f9f0dd7fbfcbeb9efa580cee02247b3869c0e90
lucas juarez <lujuarez@emeal.nttdata.com>
2025-04-29 17:12:50 -0300

    add stalefull for slides and mapping your props

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -34,10 +34,36 @@ class AppTutorialScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(physics: const BouncingScrollPhysics(), children: [
-          
-        ],
+      body: PageView(
+        physics: const BouncingScrollPhysics(),
+        children:
+            slides
+                .map(
+                  (slideData) => _Slide(
+                    title: slideData.title,
+                    caption: slideData.caption,
+                    imageUrl: slideData.imageUrl,
+                  ),
+                )
+                .toList(),
       ),
     );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
