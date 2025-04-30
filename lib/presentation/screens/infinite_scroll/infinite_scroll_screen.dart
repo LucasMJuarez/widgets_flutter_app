692d9f67b3e9794243ee3dcf510846f66372d852
lucas juarez <lujuarez@emeal.nttdata.com>
2025-04-30 12:38:38 -0300

    create listview builder with fade in image

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -7,6 +7,19 @@ class InfiniteScroolScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Infinite Scroll and Pull to Refresh')),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return FadeInImage(
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: 300,
+            placeholder: const AssetImage('assets/images/jar-loading.gif'),
+            image: NetworkImage('https://picsum.photos/id/3/500/300'),
+          );
+        },
+      ),
+    );
   }
 }
